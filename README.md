04ee73ee8660cfc65bc70f3f35f489a656c266fc
catherinegates <catherinegates@safetyskills.com>
2020-03-05 10:18:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-a1jdet
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a1jdet)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+  })
+export class CartService {
+  items = [];
+  constructor() { }
+
+  addToCart(product) {
+    this.items.push(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+}
